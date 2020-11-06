5e54a615a02295ce5d62e7f473155e55f503661a
Amit Singh <amitsingh19975@gmail.com>
2020-11-06 19:56:17 +0530

    replacing lexical_cast

--- include/functions/cast.hpp
+++ include/functions/cast.hpp
@@ -1,29 +1,47 @@
 #if !defined(AMT_DATAFRAME_FUNCTIONS_CAST_HPP)
 #define AMT_DATAFRAME_FUNCTIONS_CAST_HPP
 
-#include <boost/lexical_cast.hpp>
 #include <series_utils.hpp>
 #include <frame_utils.hpp>
 #include <functions/transform.hpp>
 #include <optional>
+#include <sstream>
 
 namespace amt {
 
 namespace fn {
+
+    namespace detail{
+        template<typename To, typename U>
+        std::optional<To> cast_helper(U&& val) {
+            static std::stringstream ss;
+            ss.str("");
+            ss << std::forward<U>(val);
+            To temp;
+            ss >> temp;
+            if(ss.fail()){
+                return std::nullopt;
+            }else{
+                return { std::move(temp) };
+            }
+        }
+    }
 
 template <typename To> struct cast_t {
     template <Box BoxType> constexpr BoxType operator()(BoxType const &b) const {
         BoxType res( dtype<To>{} );
         visit(b, [&res]<typename T>(T &&val) {
             if constexpr (!is_monostate_v<T>) {
-                if constexpr (std::numeric_limits<To>::has_quiet_NaN) {
-                    try {
-                        res = boost::lexical_cast<To>(val);
-                    } catch (...) {
+                auto op = detail::cast_helper<To>(std::forward<T>(val));
+                if(op) res = std::move(*op);
+                else{
+                    if constexpr (std::numeric_limits<To>::has_quiet_NaN) {
                         res = To(NAN);
+                    } else {
+                        throw std::runtime_error(
+                            ERR_CSTR("amt::cast_t::operator()(BoxType const &) : bad cast")
+                        );
                     }
-                } else {
-                    res = boost::lexical_cast<To>(val);
                 }
             } else {
                 res = To{};
@@ -35,7 +53,7 @@ template <typename To> struct cast_t {
     template <Series SeriesType, Series OutSeriesType>
     constexpr OutSeriesType &operator()(SeriesType const &in, OutSeriesType &out) const {
         if constexpr(PureSeries<OutSeriesType>){
-            detail::set_dtype(out,dtype<To>());
+            ::amt::detail::set_dtype(out,dtype<To>());
         }
         transform(in, out,
                          [this](auto &&val) { return this->operator()(val); });
@@ -82,13 +100,14 @@ template <typename To> struct cast_t {
 template <typename To> struct try_cast_t {
     template <Box BoxType>
     constexpr std::optional<BoxType> operator()(BoxType const &b) const {
-        return visit(b, []<typename T>(T const &val) -> std::optional<BoxType> {
+        return visit(b, [this]<typename T>(T const &val) -> std::optional<BoxType> const {
             if constexpr (is_monostate_v<T>) {
                 return std::nullopt;
             } else {
-                try {
-                    return {boost::lexical_cast<To>(val)};
-                } catch (...) {
+                auto op = detail::cast_helper<To>(val);
+                if(op){
+                    return {std::move(*op)};
+                }else{
                     if constexpr (std::numeric_limits<To>::has_quiet_NaN) {
                         return {NAN};
                     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <boost/core/demangle.hpp>
 #include <iostream>
 #include <series.hpp>
 #include <functions.hpp>
@@ -6,10 +5,6 @@
 #include <algorithm>
 #include <traits/pipeline_traits.hpp>
 #include <frame.hpp>
-
-std::string get_name(auto&& t){
-    return boost::core::demangle(typeid(t).name());
-}
 
 void print(std::unordered_set<amt::box> const& s){
     for(auto const& el : s){
@@ -24,16 +19,17 @@ void print(std::unordered_map<amt::box,std::size_t> const& s){
 }
 
 int main(){
-    amt::frame f = {
-        {"Test1",{1.,2.,3.345,4.}, amt::dtype<double>{}},
-        {"Test2",{"1","23333333333333333333333333333333333333333333333333333","3","4"}, amt::dtype<std::string>{}},
-        {"Test3",{"1","2","3","4"}, amt::dtype<std::string>{}}
-    };
+    // amt::frame f = {
+    //     {"Test1",{1.,2.,3.345,4.}, amt::dtype<double>{}},
+    //     {"Test2",{"1","23333333333333333333333333333333333333333333333333333","3","4"}, amt::dtype<std::string>{}},
+    //     {"Test3",{"1","2","3","4"}, amt::dtype<std::string>{}}
+    // };
 
     // std::cout<<amt::list_filter(f,amt::name_list{"Test1", "Test3"})<<'\n';
 
-    // amt::series temp = {"names", {"A", "A", "B", "A", "C", "C"}, amt::dtype<std::string>()};
-    std::cout<< amt::describe(f)<<'\n';
+    amt::series temp = {"names", {"A", "A", "B", "A", "C", "C"}, amt::dtype<std::string>()};
+    // std::cout<< amt::describe(f)<<'\n';
+    std::cout<<amt::cast<char>(temp)<<'\n';
 
     return 0;
 }
