b6be350e5cf1094aac4603f7d5e89ae929fc52d7
Amit Singh <amitsingh19975@gmail.com>
2021-05-14 19:28:34 +0530

    remove noexcept

--- include/dataframe/functions/insert.hpp
+++ include/dataframe/functions/insert.hpp
@@ -13,14 +13,14 @@ namespace amt::fn {
 
         template< typename T, traits::BoundedTypeStorage S >
         requires( !std::decay_t< S >::is_static_storage ) constexpr auto
-        operator()( S&& s, size_type pos, T&& val ) const noexcept {
+        operator()( S&& s, size_type pos, T&& val ) const {
             auto& cont = ::amt::get< T >( s );
             this->helper( cont, pos, std::forward< T >( val ) );
         }
 
         template< typename T, traits::UnboundedTypeStorage S >
         requires( !std::decay_t< S >::is_static_storage ) constexpr auto
-        operator()( S&& s, size_type pos, T&& val ) const noexcept {
+        operator()( S&& s, size_type pos, T&& val ) const {
             auto& cont = s.template get_or_make< T >().value();
             this->helper( cont, pos, std::forward< T >( val ) );
         }
@@ -28,7 +28,7 @@ namespace amt::fn {
         template< typename T, traits::BoundedTypeStorage S >
         requires( !std::decay_t< S >::is_static_storage ) constexpr auto
         operator()( S&& s, size_type pos,
-                    std::initializer_list< T > li ) const noexcept {
+                    std::initializer_list< T > li ) const {
             auto& cont = ::amt::get< T >( s );
             this->helper( cont, pos, li.begin(), li.end() );
         }
@@ -36,35 +36,35 @@ namespace amt::fn {
         template< typename T, traits::UnboundedTypeStorage S >
         requires( !std::decay_t< S >::is_static_storage ) constexpr auto
         operator()( S&& s, size_type pos,
-                    std::initializer_list< T > li ) const noexcept {
+                    std::initializer_list< T > li ) const {
             auto& cont = s.template get_or_make< T >().value();
             this->helper( cont, pos, li.begin(), li.end() );
         }
 
         template< typename T, traits::Series S >
         constexpr auto operator()( S&& s, size_type pos,
-                                   T&& val ) const noexcept {
+                                   T&& val ) const {
             this->operator()( s.base(), pos, std::forward< T >( val ) );
         }
 
         template< typename T, traits::Series S >
         constexpr auto
         operator()( S&& s, size_type pos,
-                    std::initializer_list< T > li ) const noexcept {
+                    std::initializer_list< T > li ) const {
             this->operator()( s.base(), pos, li );
         }
 
     private:
         template< typename Iterator >
         constexpr auto helper( auto& cont, size_type pos, Iterator first,
-                               Iterator last ) const noexcept -> void {
+                               Iterator last ) const -> void {
             auto iter = cont.begin() + static_cast< std::ptrdiff_t >( pos );
             cont.insert( iter, first, last );
         }
 
         template< typename T >
         constexpr auto helper( auto& cont, size_type pos,
-                               T&& val ) const noexcept -> void {
+                               T&& val ) const -> void {
             auto iter = cont.begin() + static_cast< std::ptrdiff_t >( pos );
             cont.insert( iter, std::forward< T >( val ) );
         }
